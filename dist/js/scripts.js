dc1f60753b146db62132e070e1a2d5cc756ab7d1
Molly Carles <carlesm@tcd.ie>
2021-03-29 00:11:48 +0100

    Started map, stylings, and scripts

---
+++ dist/js/scripts.js
@@ -0,0 +1,47 @@
+//The below function opens and fills the location info pop-up (modal) with data for each location
+function openModal() { 
+	$("area").each(function(i){ 
+		$(this).click(function(e){
+			e.preventDefault();
+			$("#modal").hide();
+			var id = $(this).attr("id"); 
+			$.ajax({
+			  	url: "data/dataExample.json?id="+id,	//Needs to be replaced with server url so that server knows to get data pertaining to the given location's id. Currently takes data from dummy data
+			  	dataType:"json"
+			}).done(function(data) { //fills the modal with the location's data from database
+				$("#modal .title h2, #modal div.score, #modal p.description, #modal ul.comments").html("");
+				$("#modal .title h2").html(data.placeName);
+				$("#modal div.score").html("Average Score: "+data.score.average);
+				$("#modal p.description").html(data.placeDescription);
+				$("#modal button.openform").attr("data-id",id); 	//the button operates the openform function which takes a location id as a parameter, so we pass the appropriate id in here
+				data.opinions.forEach(function(opinion){
+					$("#modal ul.comments").append('<li><p>'+opinion.comments+'</p><p class="signature">'+opinion.userName+'</p></li>') //format how the specific part to each comment are displayed with the appropriate data
+				});
+				$("#modal").show(100);
+				openForm(data);
+			}).fail(function(xhr){
+				console.log(xhr.responseText);
+			});
+		});
+	});
+}
+
+function closeModal() {
+	$("#modal .close").click(function(e){
+		e.preventDefault();
+		$("#modal").hide(100);
+	});
+}
+
+function openForm(data) {
+	$("#modal button.openform").click(function(){
+		$("#modal").css({"top":"0.3%"})
+		$("#modal .info").hide();
+		$("#modal form h2 span").html(data.placeName);
+		$("#modal form input[name='id']").val(data.id);
+		$("#modal form").show();
+	});
+}
+
+openModal();
+closeModal();
