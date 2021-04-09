6df8b8a00ba492f223f33a936f4c4d830ef4dc5a
AprilS21 <asheeran@tcd.ie>
2021-04-09 15:21:00 +0100

    update to filter (not finished)

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -43,26 +43,8 @@ function openForm(data) {
 	});
 }
 
-function menu(){
-	var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+function myfunct(){
+	console.log("submitted");
 }
 openModal();
 closeModal();
-menu();
